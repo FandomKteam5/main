b642d8d39cb0e650d4b985926a776c0ce7a1583f
KimTaeHun <kth32105@naver.com>
2024-10-25 22:02:07 +0900

    Fix: App을  Router로 변경

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import './styles/global.css';
-import App from './App';
+import Router from './Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Router />);
