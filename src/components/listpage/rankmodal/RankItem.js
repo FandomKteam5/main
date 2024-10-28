fa216b0def82c98e0b4a196d7a8069e18df2af5c
장수경 <jangsookyeong@jangsoogyeong-ui-MacBookAir.local>
2024-10-28 09:14:46 +0900

    Feat: 이달의 차트 모달 창 구현 시작

---
+++ src/components/listpage/rankmodal/RankItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const RankItem = ({ rank, idol, isSelected, onSelect }) => {
+  return (
+    <div className="rank-item" onClick={() => onSelect(idol.name)}>
+      <img src={idol.image} alt={`${idol.name} profile`} />
+      <div className="rank-details">
+        <p>
+          {rank}. {idol.name}
+        </p>
+        <p>{idol.votes} 표</p>
+      </div>
+      <input type="radio" checked={isSelected} onChange={() => {}} />
+    </div>
+  );
+};
+
+export default RankItem;
